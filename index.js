0ece6f593e6fd65ee1088b13a18886b3dd7feff7
yatsun <yatsunlam25@gmail.com>
2021-09-14 00:40:37 -0400

    commit tree 2f87afbaf824182a15b23577a9b5234ba267cca7

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    // let i = 0;
+    // while i < 2 do {
+    //     return drivers[i];
+    //     i++;
+    // }
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2, 4)
+    // return drivers.slice(-2)
+};
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+const createFareMultiplier = function(numInt){
+    return function(cost) {
+        return numInt * cost;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
